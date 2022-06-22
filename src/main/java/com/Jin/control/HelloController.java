343fa4f12491fcbbb8fa1fd63cadea6e95386389
admin <2465010243@qq.com>
2022-06-22 15:56:02 +0800

    first changed

---
+++ src/main/java/com/Jin/control/HelloController.java
@@ -0,0 +1,15 @@
+package com.Jin.control;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @ResponseBody
+    @RequestMapping("/hello")
+    public String hello(){
+        return "Hello EmialCheck （git）";
+    }
+}
---
+++ src/test/java/com/Jin/EmailCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Jin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailCheckApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
